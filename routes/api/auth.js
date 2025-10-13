d3a322d18cd3521aeb547f38690026492f1bcae7
becoder100 <rk9058740@gmail.com>
2025-10-13 18:10:59 +0530

    makign middleware for authorisation auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,6 @@
 const express=require('express');
 const router=express.Router();
+const middleware=require('../../middleware/auth');
+
 router.get('/',(req,res)=>res.send('auth route' ));
 module.exports=router;
