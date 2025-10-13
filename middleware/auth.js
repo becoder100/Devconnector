7d26f5a35a63c3ea19ffaba708ebfe1566eb2017
becoder100 <rk9058740@gmail.com>
2025-10-13 19:08:33 +0530

    upadated auth.js to authenticate user

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,4 +8,11 @@ module .exports=function(req,res,next){
         return res.status(401).json({ msg:'No token, authorization denied' });
     }
     //verify token
+    try{
+        const decoded=jwt.verify(token,config.get('jwtSecret'));
+        req.user=decoded.user;
+        next();
+    }catch(err){
+        res.status(401).json({ msg:'Token is not valid' });
+    }
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,67 @@
 const express=require('express');
+const bcrypt=require('bcryptjs');
 const router=express.Router();
-const middleware=require('../../middleware/auth');
+const auth=require('../../middleware/auth');
+const User=require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const jwt=require('jsonwebtoken');
+const config=require('config');
 
-router.get('/',(req,res)=>res.send('auth route' ));
+router.get('/',auth, async(req,res)=>{
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+
+    }catch(err){
+      res.send('Server Error');
+    }
+});
+//post api/auth
+//desc authenticate user & get token
+//access public
+router.post('/',[
+    
+    check('email','Please include a valid email').isEmail(),
+    check('password','password is required').exists()
+],
+async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const{email,password}=req.body;
+    try{
+    // see if user exists
+    //return jsonwebtoken
+    let user=await User.findOne({ email });
+    if(!user){
+        return res.status(400).json({ errors: [{ msg: 'invalid credential' }] });
+    }
+    const isMatch=await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+    const payload={
+        user:{
+            id:user.id
+        }
+    };
+    jwt.sign(payload,config.get('jwtSecret'),
+    {expiresIn:360000},
+    (err,token)=>{
+        if(err) throw err;
+        res.json({ token });
+    });
+
+    
+
+    }catch(errors){
+        console.error(errors.message);
+        res.status(500).send('Server error');
+    }
+
+
+    
+});
 module.exports=router;
+
