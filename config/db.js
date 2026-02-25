52931f217a655fd3bf793ee2d0f62a9be67a104d
becoder100 <rk9058740@gmail.com>
2026-02-25 22:26:54 +0530

    Remove config package

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db=config.get('mongoURI');
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(db);
-        console.log('MongoDB connected');
-    }catch(err){
-        console.error(err.message);
-        process.exit(1);
-    }
-}
-module.exports=connectDB;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
