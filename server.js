3beb9031312bd40309eebd8bceac4d93042b40c7
becoder100 <rk9058740@gmail.com>
2026-02-25 18:01:31 +0530

    build successfully

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-    const express = require('express');
-    const port = process.env.PORT || 5000;
-    const connectDB = require('./config/db');
-    const path = require('path');
-    const app = express();
-    connectDB();
-    //init middleware
-    app.use(express.json({ extended: false }));
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-    app.use('/api/users', require('./routes/api/users'));
-    app.use('/api/auth', require('./routes/api/auth'));
-    app.use('/api/profile', require('./routes/api/profile'));
-    app.use('/api/posts', require('./routes/api/posts'));
+const app = express();
 
-    //serve static assets in production
-    if (process.env.NODE_ENV === 'production') {
-  // Set static folder
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, 'client', 'build', 'index.html')
-    );
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
-    const PORT = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
